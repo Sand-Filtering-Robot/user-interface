7bbc8836f07f6acd303400ca3ad1d0eda2f92708
stephensj7426 <stephensj7426@gmail.com>
2024-04-12 16:34:18 -0400

    Add frontend (needs updates), collect online resources (except for google maps)

--- app.py
+++ app.py
@@ -4,11 +4,31 @@ app = Flask(__name__, static_url_path="")
 
 @app.route('/', methods=['GET'])
 def root():
-    return redirect('/home')
+    return redirect('/index.html')
 
-@app.route('/home', methods=['GET'])
+@app.route('/index.html', methods=['GET'])
 def home_page():
     return render_template('index.html')
+
+@app.route('/about-us.html', methods=['GET'])
+def about_us():
+    return render_template('about-us.html')
+
+@app.route('/map.html', methods=['GET'])
+def map():
+    return render_template('map.html')
+
+@app.route('/remote-control.html', methods=['GET'])
+def remote_control():
+    return render_template('remote-control.html')
+
+@app.route('/user-guideline.html', methods=['GET'])
+def user_guideline():
+    return render_template('user-guideline.html')
+
+@app.route('/sand-es-status.html', methods=['GET'])
+def status():
+    return render_template('sand-es-status.html')
 
 @app.errorhandler(404)
 def not_found(error):
@@ -18,7 +38,7 @@ def not_found(error):
         description: Endpoint to return a not found 404 page.
         responses: Returns 404 object.
     """
-    return make_response(jsonify({'error': 'Not found'}), 404)
+    return render_template('404.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host="127.0.0.1", port=5000)
