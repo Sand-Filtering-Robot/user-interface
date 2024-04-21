599f302a08f6444f91205829833910ab34bfca4e
stephensj7426 <stephensj7426@gmail.com>
2024-04-20 22:32:42 -0400

    Handling for mode selection and speed

--- motor_interface.py
+++ motor_interface.py
@@ -1,23 +1,15 @@
-from enum import Enum
-import socket
-
-class Command(Enum):
-    AUTO = 1
-    MANUAL = 2
-    MOVE = 3
-    STOP = 4
-    SPEED = 5
-    LEFT = 6
-    RIGHT = 7
-    UP = 8
-    DOWN = 9
 
 # maybe we can change it so that each type of command has its own function
 def send_motor_command(clientSocket, cmd, values):
-    print(f"Sending: {cmd}:{values}")
 
+    command_type = 'MOVE'
+    if (cmd in ['MANUAL', 'AUTONOMOUS']):
+        command_type = 'MODE'
+    elif (cmd in ['FASTER', 'SLOWER']):
+        command_type = 'SPEED'
+    print(f"Sending: {command_type}:{cmd}:{values}")
     # utilize global socket to send the command
-    clientSocket.send(f'MOVE {cmd}'.encode())
+    clientSocket.send(f'{command_type} {cmd}'.encode())
 
     # expect an ACK from server :) (debug purposes)
     msg = clientSocket.recv(64).decode()
@@ -25,7 +17,3 @@ def send_motor_command(clientSocket, cmd, values):
         print('Got ACK. Everything is good')
     else:
         print('Bad Response from server x.x')
-
-    #mysock = socket.create_connection(("127.0.0.1", 8080))
-    # mysock.send(f"{cmd}:values")
-    #mysock.close()
